5bbd8d996316980325f42861a4bb671b383adbec
jackson-waschura <jwaschur@calpoly.edu>
2020-10-21 20:28:42 -0700

    fixed broken App (did not drag)

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +7,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Draggable>
+	  <div>
+	    <img src={logo} className="App-logo" alt="logo" />
+          </div>
+	</Draggable>
+	<Draggable>
+	  <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+	</Draggable>
         <a
           className="App-link"
           href="https://reactjs.org"
